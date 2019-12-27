40fce3e55e3d4980bf0914827bbf3a50ae360db5
Carloslhc <carlosh21r4@gmail.com>
2019-12-27 00:01:38 -0300

    AVANCES

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnInterval = 1f;
+    private float nextSpawnTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextSpawnTime)
+        {
+            // explicacion: chequeo que el tiempo sea mayor a mi tiempo del siguiente spawneo y 
+            // despues le sumo al siguiente spawneo el tiempo actual mas 1 segundo (este siendo el intervalo de spawn)
+            nextSpawnTime = Time.time + spawnInterval;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,21 +7,33 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
+    // CTRL + Llave izquierda para documentacion de metodos
     void Start()
     {
-        
+        // los parametros de este metodo son el nombre del metodo a repetir en string, el tiempo en que empieza y el intervalo.
+        InvokeRepeating("SpawnRandomAnimal",startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( Input.GetKeyDown( KeyCode.S ) || Input.GetButtonDown("B") ) {
+        // if ( Input.GetKeyDown( KeyCode.S ) || Input.GetButtonDown("B") ) {
+        //    SpawnRandomAnimal();
+        // }
+    }
+    
+    void SpawnRandomAnimal() {
+
+
             int animalIndex = Random.Range( 0, animalPrefabs.Length );
         
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX), 0, spawnPosZ );
 
             Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation );
-        }
     }
+
 }
