d47b2f4b088d57224b8122bf10bf7ee5c11c6f95
Carloslhc <carlosh21r4@gmail.com>
2020-01-06 22:04:25 -0300

    avances en curso

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb; 
-    // Start is called before the first frame update
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision) {
+
+        if (collision.gameObject.CompareTag("Ground")){
+            isOnGround = true;
+
+        } else if (collision.gameObject.CompareTag("Obstacle")) {
+            gameOver = true;
+            Debug.Log("Game Over!");
         }
     }
 }
---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25,0,0);
+    private float startDelay = 2;
+    private float delayRate = 2;
+    private PlayerController PlayerControllerScript;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, delayRate);
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        
+    }
+    void SpawnObstacle(){
+        if (!PlayerControllerScript.gameOver) {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+
+    }
+}
