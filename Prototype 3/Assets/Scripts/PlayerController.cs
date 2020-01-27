1c602de976a1afbe3b2e28e492b922fe125faffb
Carloslhc <carlosh21r4@gmail.com>
2020-01-26 23:49:34 -0300

    particulas y sonidos junto con archivos inciales de challenge

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,37 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    private Animator playerAnim; 
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtSplatter;
     public float jumpForce = 10.0f;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
+            dirtSplatter.Stop();
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            
         }
     }
     private void OnCollisionEnter(Collision collision) {
 
         if (collision.gameObject.CompareTag("Ground")){
+            dirtSplatter.Play();
             isOnGround = true;
 
         } else if (collision.gameObject.CompareTag("Obstacle")) {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAudio.PlayOneShot(crashSound, 1.0f);
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            dirtSplatter.Stop();
+            explosionParticle.Play();
         }
     }
 }
