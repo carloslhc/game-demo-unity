3491cbf68bb4d02321b88ef2a9473975606f8144
Carloslhc <carlosh21r4@gmail.com>
2020-02-19 23:42:25 -0300

    avance en prototype 4

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point"); // usar esto para buscar un gameobject de la escena
+    }
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        // playerRb.AddForce(Vector3.forward * speed * forwardInput);
+    }
+}
