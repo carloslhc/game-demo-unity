9edfa202269c7b90b6af156b3e08a32e295b30f0
Carloslhc <carlosh21r4@gmail.com>
2020-02-21 22:26:26 -0300

    protoype 4 spawn player y mas

---
+++ Prototype 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 loodDirection = (player.transform.position - transform.position).normalized;
+
+        // This is to follow the player normalized is for slow down de force applied in the vector
+        enemyRb.AddForce(loodDirection * speed);
+    }
+}
